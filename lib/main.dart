a645df17f3a05a2caf136d0fcf67dd6e3a304614
TejasP07 <88932277+TejasP07@users.noreply.github.com>
2022-10-16 22:33:06 +0530

    Commit

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      );
+  }
+
+  Container buildMyNavBar(BuildContext context) {
+    return Container(
+    
+      ),
+     
+  }
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
+
